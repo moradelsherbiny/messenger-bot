f994e54b296c7284ff22fccf65fb3da3b5b763ed
moradelsherbiny <moradelsherbinyy@gmail.com>
2026-06-01 23:31:37 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -15,12 +15,17 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
-const PAGE_ACCESS_TOKEN = "PASTE_YOUR_PAGE_ACCESS_TOKEN_HERE";
+const PAGE_ACCESS_TOKEN = "EAAMGscNpjaYBRimKcGnLyjwf637ksnWIafM6OXZBR8MXqpMnwnRiqnnF3FF599ZBVR2ZCYCHCDIQvK3Q7JMpFMFWcGVE1P173wCLZCBAZBt39yvff8Rw42BMPeQPrJD2otXMlGzYmPGE9SNqPMqWuYKGdMTinh3FSLuZBI0iXqwtCiJtn9DhiDlRyZAx861xgE4p8B1PQZDZD";
 const VERIFY_TOKEN = "MY_VERIFY_TOKEN_123";
 
 // ✅ رسايل كل بوست — الرسالة الأولى دايمًا صورة
 const POST_MESSAGES = {
-
+  "1545484863816450": [
+    { type: "image", url: "https://i.postimg.cc/TPGR0JWk/710556252-1545484950483108-4579957299604862568-n.jpg", delay: 0 },
+    { type: "text", text: "الجَمبسوت الأسود السادة رجع تاني 🖤✨\n\nخامة Waterproof مستورد مناسبة للبحر والصيف 🌊\nالطقم مكوّن من:\nجمبسوت + جيبة سوداء Wrap بتتربط + تربون 👒\n\n📏 المقاسات: من S لحد 4XL\n💰 السعر: 750 جنيه فقط\n\nولو حابة تكمّلي اللوك وتضيفي الكاش التريكو ✨\nمتاح بسعر إضافي: 400 جنيه\n\nلوك شيك ومقفل وعملي جدًا للبحر والخروجات الصيفية 🖤\nاطلبي قبل نفاد الكمية", delay: 2 },
+    { type: "text", text: "Small       40:50\nMedium  50 : 60 \nLarge       60 : 70 \nXl             70 : 80 \nXxl            80 : 90 \n3xl             90:100 \n4xl             100:110 \n", delay: 3 },
+    { type: "text", text: "لو تحبي اساعدك ف اختيار المقاس انا هكون مع حضرتك عيوني ليكي😍", delay: 4 }
+  ]
 };
 
 // ✅ Webhook Verification
